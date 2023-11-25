55d265fde2974898e384ab428d791c02224d27a7
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2023-11-25 09:10:49 +0100

    Collinearpoints

---
+++ 1.3_CollinearPoints/BruteCollinearPoints.java
@@ -0,0 +1,24 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class BruteCollinearPoints {
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    public int numberOfSegments() {
+        return -1;
+    }
+
+    public LineSegment[] segments() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 1.3_CollinearPoints/FastCollinearPoints.java
@@ -0,0 +1,23 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class FastCollinearPoints {
+    // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points) {
+    }
+
+    public int numberOfSegments() {
+        return -1;
+    }
+
+    public LineSegment[] segments() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
