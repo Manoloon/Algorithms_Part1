5789845df7aca5f331df5a434f3577057bb97d5f
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2023-11-27 14:39:45 -0300

    Update BruteCollinearPoints.java

--- 1.3_CollinearPoints/BruteCollinearPoints.java
+++ 1.3_CollinearPoints/BruteCollinearPoints.java
@@ -7,7 +7,12 @@
 public class BruteCollinearPoints {
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
-
+			if(points == null){
+				throw new IllegalArgumentException();
+			}
+			for(point : points){
+				if(point == null) throw new IllegalArgumentException();
+				}
     }
 
     public int numberOfSegments() {
