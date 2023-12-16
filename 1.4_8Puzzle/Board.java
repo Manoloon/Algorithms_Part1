fef18bfdbda09ec2744df0db25afd726ecb3bcaa
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2023-12-16 08:54:25 +0100

    board

--- 1.4_8Puzzle/Board.java
+++ 1.4_8Puzzle/Board.java
@@ -5,24 +5,51 @@
  **************************************************************************** */
 
 public class Board {
+    private int[][] tiles;
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
+        this.tiles = new int[tiles.length][tiles[0].length];
+
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles[i].length; j++) {
+                this.tiles[i][j] = tiles[i][j];
+            }
+        }
     }
 
     // string representation of this board
     public String toString() {
-        return "";
+        StringBuilder str = new StringBuilder();
+        int n = tiles.length;
+        str.append(n).append("\n");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                str.append(tiles[i][j]).append(" ");
+            }
+            str.append("\n");
+        }
+        return str.toString();
     }
 
     // board dimension n
     public int dimension() {
-        return -1;
+        return tiles.length;
     }
 
     // number of tiles out of place
     public int hamming() {
+        int result;
+        int n = tiles.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (tiles[i][j] == 0)
+                    continue;
+                // if (tiles[i][j] == 0)
+            }
+        }
         return -1;
     }
 
